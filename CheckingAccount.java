38a650f7ffa7e0573e5b5c8d7a3b0235986180ca
Dvja50 <deja.randolph@bsu.edu>
2025-03-18 20:27:38 +0000

    process

---
+++ CheckingAccount.java
@@ -0,0 +1,22 @@
+ public class CheckingAccount implements HasMenu
+ 
+    double balance
+ 
+  public CheckingAccount()
+	this.balance = 0.0;
+
+  public CheckingAccount(double balance)
+    main()
+    string menu()
+    void start()   
+    double getBalance()
+    string getBalanceString()
+   public void setBalance(double balance)
+   public void checkBalance()
+
+    private double getDouble()
+
+   public  void makeDeposit()
+
+   public void makeWithdrawal
+
